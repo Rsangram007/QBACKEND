0e6c3f99638b55c3bc88db1e5d85299db87a3ba8
Rsangram007 <sangrambeheraa007@gmail.com>
2024-09-17 16:41:51 +0530

    Done

--- src/index.js
+++ src/index.js
@@ -27,6 +27,10 @@ sequelize.authenticate()
         console.error('Unable to connect to the database:', err);
     });
 
+app.get ("/",(req,res)=>{
+    res.send("Welcome")
+})
+
 const PORT = process.env.PORT || 3000
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
